9459a376c08eab10e75a600f37bfa305391a43b2
coutinho21 <guilhermecscoutinho@gmail.com>
2022-12-26 16:36:18 +0000

    Added airline class

---
+++ src/Airline.h
@@ -0,0 +1,15 @@
+#ifndef AIRLINE_H
+#define AIRLINE_H
+
+#include <string>
+
+using namespace std;
+
+class Airline {
+private:
+    string code, name, callsign, country;
+public:
+    Airline();
+};
+
+#endif
