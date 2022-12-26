0b7acfa2419207929252da1a3bdd7c00c102b101
coutinho21 <guilhermecscoutinho@gmail.com>
2022-12-26 16:36:54 +0000

    Added flight class

---
+++ src/Flight.h
@@ -0,0 +1,16 @@
+#ifndef FLIGHT_H
+#define FLIGHT_H
+
+#include <string>
+
+using namespace std;
+
+class Flight {
+private:
+    string source, target, airline;
+    //maybe change the types of the fields to the actual classes of the airports and airline
+public:
+    Flight();
+};
+
+#endif
